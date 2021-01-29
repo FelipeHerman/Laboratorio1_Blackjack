6abddae57c2410dbcaa9c2df889f96b73b7acb14
Felipe Herman <j_felipe05@hotmail.com>
2021-01-29 18:04:02 -0500

    Valor de cada carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -46,3 +46,23 @@ const pedirCarta = () => {
 } 
 
 //pedirCarta();
+
+// Valor de la carta
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length-1);
+
+    return (isNaN(valor)) ? 
+            (valor === 'A') ? 11 : 10
+            : valor * 1;
+    
+    /* if (isNaN(valor)) {
+        puntos = (valor === 'A') ? 11 : 10;
+    }else{
+        puntos = valor * 1;
+    }
+
+    console.log(puntos) */
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({valor})
